d89cc3fbf1702105b8677a660a84f8a4da714bfb
Zarko Stanisic <zarko@tcom.rs>
2022-03-31 14:55:29 +0200

    edit and create post

--- src/views/Posts/EditPost.js
+++ src/views/Posts/EditPost.js
@@ -210,135 +210,131 @@ class EditPost extends Component {
     return (
       <>
         <div className="content">
-          <Nav tabs>
-             <NavItem>
-               <NavLink
-                 className={classnames({ active: this.state.activeTab === 'info' })}
-                 onClick={() => { this.toggle('info'); }}
-               >
-                 Osnovno
-               </NavLink>
-            </NavItem>
-            <NavItem>
-               <NavLink
-                 className={classnames({ active: this.state.activeTab === 'images' })}
-                 onClick={() => { this.toggle('images'); }}
-               >
-                 Slike
-               </NavLink>
-             </NavItem>
-           </Nav>
-            <TabContent activeTab={this.state.activeTab}>
-              <TabPane tabId="info">
-                <Row>
-                  <Col md="10">
-                    <Card className="card-post">
-                      <CardHeader>
-                        <CardTitle tag="h5">Izmena objave</CardTitle>
-                      </CardHeader>
-                      <CardBody>
-                        <Form>
-                          <Row>
-                            <Col md="8">
+        <Card className="card-post">
+          <CardHeader>
+            <CardTitle tag="h5">Izmena objave</CardTitle>
+          </CardHeader>
+          <CardBody>
+            <Nav tabs>
+               <NavItem>
+                 <NavLink
+                   className={classnames({ active: this.state.activeTab === 'info' })}
+                   onClick={() => { this.toggle('info'); }}
+                 >
+                   Osnovno
+                 </NavLink>
+              </NavItem>
+              <NavItem>
+                 <NavLink
+                   className={classnames({ active: this.state.activeTab === 'images' })}
+                   onClick={() => { this.toggle('images'); }}
+                 >
+                   Slike
+                 </NavLink>
+               </NavItem>
+             </Nav>
+              <TabContent activeTab={this.state.activeTab}>
+                <TabPane tabId="info">
+                  <Row>
+                    <Col md="12" className="pt-5">
+                      <Form>
+                        <Row>
+                          <Col md="12">
+                          <FormGroup>
+                            <Label for="first_name">Naslov</Label>
+                            <Input
+                              type="title"
+                              name="title"
+                              value={this.state.title} 
+                              onChange={this.handleChange}
+                            />
+                            <FormText color="muted">
+                              {this.validator.message('naslov', this.state.title, 'required|alpha_space')}
+                              {this.state?.error_message_title}
+                            </FormText>
+                          </FormGroup>
+                          </Col>
+                          
+                          <Col md="4">
                             <FormGroup>
-                              <Label for="first_name">Naslov</Label>
-                              <Input
-                                type="title"
-                                name="title"
-                                value={this.state.title} 
+                              <Label for="category_id">Kategorija</Label>
+                              <Input type="select"
+                                name="category_id" value={this.state.category_id} 
                                 onChange={this.handleChange}
-                              />
-                              <FormText color="muted">
-                                {this.validator.message('naslov', this.state.title, 'required|alpha_space')}
-                                {this.state?.error_message_title}
+                              >
+                                {categories}
+                              </Input>
+                              <FormText>
+                                {this.state?.error_message_category_id}
                               </FormText>
                             </FormGroup>
-                            </Col>
+                          </Col>
+                        </Row>
+                        
+                        <Row>
+                          <Col md="12">
+                            <FormGroup>
+                              <Label for="text">Tekst</Label>
+                              <CKEditor
+                                  name="text"
+                                  config={ {
+                                    toolbar: [ 'heading', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote', '|', 'Undo', 'Redo' ]
+                                  } }
+                                  editor={ ClassicEditor }
+                                  data={this.state.text} 
+                                  onChange={ ( event, editor ) => {
+                                      const data = editor.getData();
+                                      this.setState({text: data});
+                                  } }
+                              />
+                              <FormText color="muted">
+                                {this.validator.message('tekst', this.state.text, 'required')}
+                                {this.state?.error_message_text}
+                              </FormText>
+                            </FormGroup>
+                          </Col>
+                        </Row>
+                        
+                        <Row>
+                          <div className="update ml-auto mr-auto">
                             
-                            <Col md="4">
-                              <FormGroup>
-                                <Label for="category_id">Kategorija</Label>
-                                <Input type="select"
-                                  name="category_id" value={this.state.category_id} 
-                                  onChange={this.handleChange}
-                                >
-                                  {categories}
-                                </Input>
-                                <FormText>
-                                  {this.state?.error_message_category_id}
-                                </FormText>
-                              </FormGroup>
-                            </Col>
-                          </Row>
-                          
-                          <Row>
-                            <Col md="12">
-                              <FormGroup>
-                                <Label for="text">Tekst</Label>
-                                <CKEditor
-                                    name="text"
-                                    config={ {
-                                      toolbar: [ 'heading', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote', '|', 'Undo', 'Redo' ]
-                                    } }
-                                    editor={ ClassicEditor }
-                                    data={this.state.text} 
-                                    onChange={ ( event, editor ) => {
-                                        const data = editor.getData();
-                                        this.setState({text: data});
-                                    } }
-                                />
-                                <FormText color="muted">
-                                  {this.validator.message('tekst', this.state.text, 'required')}
-                                  {this.state?.error_message_text}
-                                </FormText>
-                              </FormGroup>
-                            </Col>
-                          </Row>
-                          
-                          <Row>
-                            <div className="update ml-auto mr-auto">
-                              
-                              <Button color="primary" type="button" onClick={() => this.handleUpdate()}>
-                                Sačuvaj
-                              </Button>
-                            </div>
-                          </Row>
-                        </Form>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                </Row>
-              </TabPane>
-              
-              <TabPane tabId="images">
-                <Row>
-                  <Col md="12" className="pt-5">
-                    <Card>
-                      <CardBody>
-                        <FormGroup>
-                          <input style={{ display: 'none' }} 
-                            type="file"
-                            multiple
-                            name="images" 
-                            id="images"
-                            onChange={this.fileSelectedHandler}
-                            accept="image/png,image/jpg,image/jpeg"
-                            ref={ref => this.fileInput = ref} 
-                          />
+                            <Button color="primary" type="button" onClick={() => this.handleUpdate()}>
+                              Sačuvaj
+                            </Button>
+                          </div>
+                        </Row>
+                      </Form>
+                    </Col>
+                  </Row>
+                </TabPane>
+                
+                <TabPane tabId="images">
+                  <Row>
+                    <Col md="12" className="pt-5">
+                      <FormGroup>
+                        <input style={{ display: 'none' }} 
+                          type="file"
+                          multiple
+                          name="images" 
+                          id="images"
+                          onChange={this.fileSelectedHandler}
+                          accept="image/png,image/jpg,image/jpeg"
+                          ref={ref => this.fileInput = ref} 
+                        />
 
-                          <Button color="primary" onClick={() => { if (this.fileInput) this.fileInput.click() }}><i className="icon-plus icons font-xl"></i>Izaberi slike</Button>
-                          <FormText color="muted">
-                            {this.state?.error_message_images}
-                          </FormText>
-                        </FormGroup>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                  {old_images}
-                  {images}
-                </Row>
-              </TabPane>
-            </TabContent>
+                        <Button color="primary" onClick={() => { if (this.fileInput) this.fileInput.click() }}><i className="icon-plus icons font-xl"></i>Izaberi slike</Button>
+                        <FormText color="muted">
+                          {this.state?.error_message_images}
+                        </FormText>
+                      </FormGroup>
+                    </Col>
+                    {old_images}
+                    {images}
+                  </Row>
+                </TabPane>
+              </TabContent>
+            </CardBody>
+          </Card>
         </div>
       </>
     );
--- src/views/Posts/NewPost.js
+++ src/views/Posts/NewPost.js
@@ -6,6 +6,7 @@ import SimpleReactValidator from 'simple-react-validator';
 import { fileToBase64 } from '../../components/Functions/Functions';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import classnames from 'classnames';
 
 import {
   FormGroup,
@@ -18,7 +19,12 @@ import {
   CardHeader,
   CardTitle,
   CardBody,
-  Form
+  Form,
+  TabContent,
+  TabPane,
+  Nav,
+  NavLink,
+  NavItem
 } from "reactstrap";
 
 import {Button} from "reactstrap";
@@ -29,7 +35,22 @@ class NewPost extends Component {
     title: '',
     text: '',
     category_id: '',
-    images: []
+    images: [],
+    activeTab: 'info'
+  }
+  
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+  }
+  
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
   }
   
   getCategories = () => {
@@ -84,7 +105,6 @@ class NewPost extends Component {
   }
   
   fileSelectedHandler = event => {
-      this.setState({images: []});
       
       const images = event.target.files;
       const extensions = ['image/png', 'image/jpg', 'image/jpg'];
@@ -96,10 +116,6 @@ class NewPost extends Component {
               const newFiles = [...this.state.images, result];
               this.setState({ images: newFiles});
           });
-        }else{
-          event.target.value = '';
-          this.setState({images: []});
-          alert('Format slike mora biti jpg,jpeg ili png!');
         }
       }
   };
@@ -115,112 +131,163 @@ class NewPost extends Component {
      this.getCategories();
   }
   
+  handleRemoveImage = (type, key) => {
+    let images = [...this.state.images]
+    images.splice(key, 1);
+    
+    this.setState({images: images});
+  }
+  
   render(){
     const categories = this.state.categories.map((category) => {
       return (
         <option value={category.id} key={category.id}>{category.name}</option>
       );
     });
+    
+    const images = this.state.images.map((image, key) => {
+      return (
+        <Col md="2" key={key}>
+          <Card >
+            <CardBody>
+              <div className="postImg" style={{ backgroundImage: `url('${image}')` }}></div>
+              <Button color="primary" type="button" onClick={() => this.handleRemoveImage('new', key)}>
+                Ukloni
+              </Button>
+            </CardBody>
+          </Card>
+        </Col>
+      );
+    });
   
     return (
       <>
         <div className="content">
-          <Row>
-            <Col md="8">
-              <Card className="card-post">
-                <CardHeader>
-                  <CardTitle tag="h5">Nova objava</CardTitle>
-                </CardHeader>
-                <CardBody>
-                  <Form>
-                    <Row>
-                      <Col md="8">
+        <Card className="card-post">
+          <CardHeader>
+            <CardTitle tag="h5">Nova objava</CardTitle>
+          </CardHeader>
+          <CardBody>
+            <Nav tabs>
+               <NavItem>
+                 <NavLink
+                   className={classnames({ active: this.state.activeTab === 'info' })}
+                   onClick={() => { this.toggle('info'); }}
+                 >
+                   Osnovno
+                 </NavLink>
+              </NavItem>
+              <NavItem>
+                 <NavLink
+                   className={classnames({ active: this.state.activeTab === 'images' })}
+                   onClick={() => { this.toggle('images'); }}
+                 >
+                   Slike
+                 </NavLink>
+               </NavItem>
+             </Nav>
+              <TabContent activeTab={this.state.activeTab}>
+                <TabPane tabId="info">
+                  <Row>
+                    <Col md="12" className="pt-5">
+                      <Form>
+                        <Row>
+                          <Col md="8">
+                          <FormGroup>
+                            <Label for="first_name">Naslov</Label>
+                            <Input
+                              type="title"
+                              name="title"
+                              value={this.state.title} 
+                              onChange={this.handleChange}
+                            />
+                            <FormText color="muted">
+                              {this.validator.message('naslov', this.state.title, 'required|alpha_space')}
+                              {this.state?.error_message_title}
+                            </FormText>
+                          </FormGroup>
+                          </Col>
+                          
+                          <Col md="4">
+                            <FormGroup>
+                              <Label for="category_id">Kategorija</Label>
+                              <Input type="select"
+                                name="category_id" value={this.state.category_id} 
+                                onChange={this.handleChange}
+                              >
+                                {categories}
+                              </Input>
+                              <FormText>
+                                {this.state?.error_message_category_id}
+                              </FormText>
+                            </FormGroup>
+                          </Col>
+                        </Row>
+                        
+                        <Row>
+                          <Col md="12">
+                            <FormGroup>
+                              <Label for="text">Tekst</Label>
+                              <CKEditor
+                                  name="text"
+                                  config={ {
+                                    toolbar: [ 'heading', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote', '|', 'Undo', 'Redo' ]
+                                  } }
+                                  editor={ ClassicEditor }
+                                  data={this.state.text} 
+                                  onChange={ ( event, editor ) => {
+                                      const data = editor.getData();
+                                      this.setState({text: data});
+                                  } }
+                              />
+                              <FormText color="muted">
+                                {this.validator.message('tekst', this.state.text, 'required')}
+                                {this.state?.error_message_text}
+                              </FormText>
+                            </FormGroup>
+                          </Col>
+                        </Row>
+                        
+                        <Row>
+                          <div className="update ml-auto mr-auto">
+                            
+                            <Button color="primary" type="button" onClick={() => this.handleSave()}>
+                              Sačuvaj
+                            </Button>
+                          </div>
+                        </Row>
+                      </Form>
+                    </Col>
+                  </Row>
+                </TabPane>
+                
+                <TabPane tabId="images">
+                  <Row>
+                    <Col md="12" className="pt-5">
+      
                       <FormGroup>
-                        <Label for="first_name">Naslov</Label>
-                        <Input
-                          type="title"
-                          name="title"
-                          value={this.state.title} 
-                          onChange={this.handleChange}
+                        <input style={{ display: 'none' }} 
+                          type="file"
+                          multiple
+                          name="images" 
+                          id="images"
+                          onChange={this.fileSelectedHandler}
+                          accept="image/png,image/jpg,image/jpeg"
+                          ref={ref => this.fileInput = ref} 
                         />
+
+                        <Button color="primary" onClick={() => { if (this.fileInput) this.fileInput.click() }}><i className="icon-plus icons font-xl"></i>Izaberi slike</Button>
                         <FormText color="muted">
-                          {this.validator.message('naslov', this.state.title, 'required|alpha_space')}
-                          {this.state?.error_message_title}
+                          {this.state?.error_message_images}
                         </FormText>
                       </FormGroup>
-                      </Col>
-                      
-                      <Col md="4">
-                        <FormGroup>
-                          <Label for="category_id">Kategorija</Label>
-                          <Input type="select"
-                            name="category_id" value={this.state.category_id} 
-                            onChange={this.handleChange}
-                          >
-                            {categories}
-                          </Input>
-                          <FormText>
-                            {this.state?.error_message_category_id}
-                          </FormText>
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    
-                    <Row>
-                      <Col md="10">
-                        <FormGroup>
-                          <Label for="images">Slike</Label>
-                          <Input type="file"
-                            multiple
-                            name="images" 
-                            id="images"
-                            onChange={this.fileSelectedHandler}
-                            accept="image/png,image/jpg,image/jpeg"
-                          ></Input>
-                          <FormText color="muted">
-                            {this.state?.error_message_images}
-                          </FormText>
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    
-                    <Row>
-                      <Col md="12">
-                        <FormGroup>
-                          <Label for="text">Tekst</Label>
-                          <CKEditor
-                              name="text"
-                              config={ {
-                                toolbar: [ 'heading', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote', '|', 'Undo', 'Redo' ]
-                              } }
-                              editor={ ClassicEditor }
-                              data={this.state.text} 
-                              onChange={ ( event, editor ) => {
-                                  const data = editor.getData();
-                                  this.setState({text: data});
-                              } }
-                          />
-                          <FormText color="muted">
-                            {this.validator.message('tekst', this.state.text, 'required')}
-                            {this.state?.error_message_text}
-                          </FormText>
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    
-                    <Row>
-                      <div className="update ml-auto mr-auto">
-                        
-                        <Button color="primary" type="button" onClick={() => this.handleSave()}>
-                          Sačuvaj
-                        </Button>
-                      </div>
-                    </Row>
-                  </Form>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
+                    </Col>
+                    {images}
+                  </Row>
+                </TabPane>
+              </TabContent>
+          </CardBody>
+        </Card>
         </div>
       </>
     );
